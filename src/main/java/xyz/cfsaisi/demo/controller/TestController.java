6432f1ee5de48284c27a5c974ebc09c9d6371ed0
AIYYQ <yq5202016@outlook.com>
2020-11-05 19:01:22 +0800

    利用spring session + redis 解决共享session单点登录问题

---
+++ src/main/java/xyz/cfsaisi/demo/controller/TestController.java
@@ -0,0 +1,26 @@
+/**
+ * @author YYQ
+ */
+package xyz.cfsaisi.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/asd")
+public class TestController {
+
+    @GetMapping("/set")
+    public void a(HttpSession session){
+        session.setAttribute("a","共享session");
+    }
+
+    @GetMapping("/get")
+    public String b(HttpSession session){
+        Object a = session.getAttribute("a");
+        return a.toString();
+    }
+}
