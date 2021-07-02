8e82c147577354fce474b48053fe61c75aef9e75
YYQ <yyq.work@foxmail.com>
2021-07-02 17:16:41 +0800

    添加abc方法

--- src/main/java/xyz/cfsaisi/demo/controller/TestController.java
+++ src/main/java/xyz/cfsaisi/demo/controller/TestController.java
@@ -4,6 +4,7 @@
 package xyz.cfsaisi.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class TestController {
     public String b(HttpSession session){
         Object a = session.getAttribute("a");
         return a.toString();
+    }
+
+
+    @PostMapping("/abc")
+    public void abc(){
+
     }
 }
